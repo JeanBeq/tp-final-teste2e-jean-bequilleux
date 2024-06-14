9bd03a9fe0e778526324a7745efdbe5a5d128b3a
Jean B <jean.bequilleux@gmail.com>
2024-06-14 11:17:08 +0200

    test: add tests for addition, subtraction, and multiplication operations

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,11 +4,29 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5173/');
 });
 
-test('test', async ({ page }) => {
-  const button = page.locator('text=count is ');
-  const buttonText = await button.innerText();
-  console.log(buttonText);
-  expect(buttonText).toBe('count is 0');
-  await button.dblclick();
-  expect(await button.innerText()).toBe('count is 2');
+test('addition test', async ({ page }) => {
+  await page.click('button[id="1"]');
+  await page.click('button:has-text("sum")');
+  await page.click('button[id="2"]');
+  await page.click('.btnEqual');
+  const result = await page.locator('.screen').innerText();
+  expect(result).toBe('3');
+});
+
+test('soustraction test', async ({ page }) => {
+  await page.click('button[id="5"]');
+  await page.click('button:has-text("soustraction")');
+  await page.click('button[id="3"]');
+  await page.click('.btnEqual');
+  const result = await page.locator('.screen').innerText();
+  expect(result).toBe('2');
+});
+
+test('multiplication test', async ({ page }) => {
+  await page.click('button[id="2"]');
+  await page.click('button:has-text("multiplication")');
+  await page.click('button[id="3"]');
+  await page.click('.btnEqual');
+  const result = await page.locator('.screen').innerText();
+  expect(result).toBe('6');
 });
